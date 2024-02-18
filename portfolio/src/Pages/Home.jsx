141e51aace87997c0cc52fb08bab8eccb8fa4402
Akila D.A <akiladahanayake@outlook.com>
2024-02-18 23:15:36 +0000

    added home page initil dssign and code and some tweaks to navbar relating to the color

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './HomeStyling.css'
 
 const Home = () => {
   return (
     <>
+      <div className='box'>
+        <div className='landingPage'>
+          <h1 style={{ fontSize: "6em" }} >Hi</h1>
+          <h2 style={{ fontSize: "8em" }} >I'm AKILA DAHANAYAKE</h2>
+          <p style={{ fontSize: "3em" }}>I'm a front end developer,
+            <br />who can create beautiful and interactive websites, with an eye for design</p>
+        </div >
+      </div>
     </>
   )
 }
--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -3,6 +3,10 @@ import "./navbarStyling.css"
 import { Link } from 'react-router-dom'
 import { FaBars } from "react-icons/fa"
 import { FaTimes } from 'react-icons/fa'
+import { FaRegMoon } from "react-icons/fa";
+import { FaRegSun } from "react-icons/fa";
+
+
 import React, { useState } from "react"
 
 
@@ -11,8 +15,10 @@ import React, { useState } from "react"
 const navbar = () => {
 
   const [display, setDisplay] = useState(false);
-
   const handleSetDisplayclick = () => setDisplay(!display)
+
+  const [colorMode, setcolorMode] = useState(false);
+  const handlesetcolorMode = () => setcolorMode(!colorMode)
 
 
 
@@ -20,17 +26,19 @@ const navbar = () => {
     <nav className="navbar">
       <Link to={"/"}>
         <div className='navbar-logo'>
-          <img src='./Assets/logo.jpg' />
+          <img src='./Assets/logo2.jpg' />
         </div> </Link>
       <ul className={display ? "nav-menu" : "nav-menu unactive"}>
         <Link to={"/About"}><li>ABOUT</li></Link>
-        <Link to={"/Project"}><li>PROJECT</li></Link>
-        <li>GALLERY</li>
-        <Link to={"/Contact"}><li>CONTACT</li></Link>
+        <Link to={"/Project"}><li className="test">PROJECT</li></Link>
+        <Link style={{ color: "white" }}><li >GALLERY</li></Link>
+        <Link to={"/Contact"} style={{ color: "white" }}><li>CONTACT</li></Link>
       </ul>
-      <div className='hamburger' onClick={handleSetDisplayclick}>
-        {display ? (<FaBars size={30} />) : (<FaTimes size={30} />)}
+      <div className="colorModeicon" onClick={handlesetcolorMode} > {colorMode ? (<FaRegMoon size={40} style={{ justifyContent: "center" }} />) : (<FaRegSun size={40} />)}</div>
+      <div className='hamburger' style={{ color: "white" }} onClick={handleSetDisplayclick}>
+        {display ? (<FaBars size={50} />) : (<FaTimes size={50} />)}
       </div>
+
     </nav>
   )
 }
