0d6949ea1942967c0e560635eb2fe6fa52ae722a
Akila D.A <akiladahanayake@outlook.com>
2024-02-22 01:04:17 +0000

    Made a lot of changs regarding the mobile-reponsiveness but using positioons absolute and relative correctly
    switched up some classnames and the overall order of some of the html in the homepage

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -8,25 +8,25 @@ import { MdAttachEmail } from "react-icons/md";
 
 const Home = () => {
   return (
-    <>
+    <section className='main'>
       <div className='contact-details'>
         <div style={{ paddingBottom: 40 }}> <FaGithub size={50} /> </div>
         <div style={{ paddingBottom: 40 }}>  <FaLinkedin size={50} /> </div>
         <div > <MdAttachEmail size={50} />  </div>
       </div>
+      <div className='box'>
+        <div className='intro'>
+          <h1  >Hi</h1>
+          <h2  >I'm AKILA DAHANAYAKE</h2>
+          <p >I'm a front end developer,
+            <br />who can create beautiful and interactive websites, with an eye for design</p>
+        </div >
+      </div>
       <div className='subBox'>
         <div className='heroImg'></div>
         <div className='btn cv'>download CV</div>
       </div >
-      <div className='box'>
-        <div className='landingPage'>
-          <h1 style={{ fontSize: "6em" }} >Hi</h1>
-          <h2 style={{ fontSize: "8em" }} >I'm AKILA DAHANAYAKE</h2>
-          <p style={{ fontSize: "3em" }}>I'm a front end developer,
-            <br />who can create beautiful and interactive websites, with an eye for design</p>
-        </div >
-      </div>
-    </>
+    </section>
   )
 }
 
